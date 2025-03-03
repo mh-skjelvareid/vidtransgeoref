e5356be4927c79b2194f3631132643eb4bd98bee
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2025-03-03 22:12:19 +0100

    Adapting example to running on Windows

--- example_usage/process_example_data.py
+++ example_usage/process_example_data.py
@@ -1,25 +1,35 @@
+import datetime
 from pathlib import Path
+
+from exiftool import constants
 
 from vidtransgeotag import VidTransGeoTag
 
-# Paths
-# data_dir = Path("../example_data/example_input/2025-03-01_143053_BodoVidTransTest.csv")
-# video_path = Path(
-#     r"C:\Users\mha114\Dropbox\Python\vidtransgeotag\example_data\example_input\short_example_video.mp4"
-# )
-# csv_path = Path(
-#     r"C:\Users\mha114\Dropbox\Python\vidtransgeotag\example_data\example_input\2025-03-01_143053_BodoVidTransTest.csv"
-# )
+# Set the default executable to the .bat file
+constants.DEFAULT_EXECUTABLE = "exiftool.bat"
 
+# ...existing code...
+
+# Paths
+data_dir = Path("../example_data/example_input/2025-03-01_143053_BodoVidTransTest.csv")
 video_path = Path(
-    r"/home/mha114/Dropbox/Python/vidtransgeotag/example_data/example_input/short_example_video.mp4"
+    r"C:\Users\mha114\Dropbox\Python\vidtransgeotag\example_data\example_input\short_example_video.mp4"
 )
 csv_path = Path(
-    r"/home/mha114/Dropbox/Python/vidtransgeotag/example_data/example_input/2025-03-01_143053_BodoVidTransTest.csv"
+    r"C:\Users\mha114\Dropbox\Python\vidtransgeotag\example_data\example_input\2025-03-01_143053_BodoVidTransTest.csv"
 )
+
+# video_path = Path(
+#     r"/home/mha114/Dropbox/Python/vidtransgeotag/example_data/example_input/short_example_video.mp4"
+# )
+# csv_path = Path(
+#     r"/home/mha114/Dropbox/Python/vidtransgeotag/example_data/example_input/2025-03-01_143053_BodoVidTransTest.csv"
+# )
 
 print(csv_path.exists())
 
-vidtransgeotag = VidTransGeoTag(csv_file_path=csv_path)
+vidtransgeotag = VidTransGeoTag(
+    csv_file_path=csv_path, csv_time_add_offset=datetime.timedelta(hours=1)
+)
 
 vidtransgeotag.check_video_overlaps_track(video_path=video_path, verbose=True)
