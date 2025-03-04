6c5c98fe03f6094ee3c7f035e74bacfa1eede277
mh-skjelvareid <martin.hansen.skjelvareid@gmail.com>
2025-03-04 14:52:39 +0100

    Adding function for merging videos

--- vidtransgeotag/__init__.py
+++ vidtransgeotag/__init__.py
@@ -1,3 +1,3 @@
-from .vidtransgeotag import VidTransGeoTag, write_geotag_to_image
+from .vidtransgeotag import VidTransGeoTag, merge_videos_in_directory, write_geotag_to_image
 
-__all__ = ["VidTransGeoTag", "write_geotag_to_image"]
+__all__ = ["VidTransGeoTag", "merge_videos_in_directory", "write_geotag_to_image"]
